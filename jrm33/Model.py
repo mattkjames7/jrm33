df1d8fcd278e0610433d954a253c6523cba7d7ce
mattkjames7 <mattkjames7@gmail.com>
2022-05-04 23:49:50 +0100

    somemore changes to fit jrm33

--- jrm33/Model.py
+++ jrm33/Model.py
@@ -1,9 +1,9 @@
 import numpy as np
-from ._SphHarm import _SphHarm,_SphHarmScalar
+from ._SphHarm import _SphHarm
 
-def Model(r,theta,phi,MaxDeg=10):
+def Model(r,theta,phi,MaxDeg=13):
 	'''
-	JRM09 Magnetic field model (see Connerney et al 2018 below). The 
+	JRM33 Magnetic field model (see Connerney et al 2022 below). The 
 	model uses right-handed System III coordinates (I think). 
 	
 	Inputs
@@ -14,8 +14,8 @@ def Model(r,theta,phi,MaxDeg=10):
 		Colatitude in radians.
 	phi : float
 		East longitude in radians.
-	MaxDeg : int
-		Maximum degree of the model, valid 1 - 10 (default = 10).
+	Deg : int
+		Maximum degree of the model, valid 1 - 30 (default = 13).
 		
 	Returns
 	=======
@@ -26,55 +26,34 @@ def Model(r,theta,phi,MaxDeg=10):
 	Bp : float
 		Azimuthal field
 		
-	If using the JRM09 model, please cite the following paper:
+	If using the JRM33 model, please cite the following paper:
 	
-	Connerney, J. E. P., Kotsiaros, S., Oliversen, R. J., Espley, J. R., 
-	Joergensen, J. L., Joergensen, P. S., et al. (2018). A new model of 
-	Jupiter's magnetic field from Juno's first nine orbits. Geophysical 
-	Research Letters, 45, 2590– 2596. https://doi.org/10.1002/2018GL077312
-	
-	'''
-	
-	#make all the inputs arrays
-	_r = np.array([r]).flatten()
-	_t = np.array([theta]).flatten()
-	_p = np.array([phi]).flatten()
-	
-	return _SphHarm(_r,_t,_p,MaxDeg)
-	
-def ModelScalar(r,theta,phi,MaxDeg=10):
-	'''
-	JRM09 Magnetic field model (see Connerney et al 2018 below). The 
-	model uses right-handed System III coordinates (I think). 
-	
-	Inputs
-	======
-	r : float
-		Radial distance in Rj.
-	theta : float
-		Colatitude in radians.
-	phi : float
-		East longitude in radians.
-	MaxDeg : int
-		Maximum degree of the model, valid 1 - 10 (default = 10).
-		
-	Returns
-	=======
-	Br : float
-		Radial field
-	Bt : float
-		Meridional field
-	Bp : float
-		Azimuthal field
-		
-	If using the JRM09 model, please cite the following paper:
-	
-	Connerney, J. E. P., Kotsiaros, S., Oliversen, R. J., Espley, J. R., 
-	Joergensen, J. L., Joergensen, P. S., et al. (2018). A new model of 
-	Jupiter's magnetic field from Juno's first nine orbits. Geophysical 
-	Research Letters, 45, 2590– 2596. https://doi.org/10.1002/2018GL077312
+	Connerney, J. E. P., Timmins, S., Oliversen, R. J., Espley, J. R., 
+	Joergensen, J. L., Kotsiaros, S., et al. (2022). A new model of 
+	Jupiter's magnetic field at the completion of Juno's Prime Mission. 
+	Journal of Geophysical Research: Planets, 127, e2021JE007055. 
+	https://doi.org/10.1029/2021JE007055
 	
 	'''
+	
+	#get the length of the input
+	n = np.size(r)
+	
+	#whether it is an array
+	isarr = hasattr(r,'__iter__')
+	
+	if n == 1 and isarr:
+		return _SphHarm(r[0],theta[0],phi[0],Deg)
+	elif n == 1:
+		return _SphHarm(r,theta,phi,Deg)
+	else:
+		#create output arrays
+		Br = np.zeros(n,dtype='float64')
+		Bt = np.zeros(n,dtype='float64')
+		Bp = np.zeros(n,dtype='float64')
+		for i in range(0,n):
+			Br[i],Bt[i],Bp[i] = _SphHarm(r[i],theta[i],phi[i],Deg)
+	
+		return Br,Bt,Bp
 	
 
-	return _SphHarmScalar(r,theta,phi,MaxDeg)
--- jrm33/ModelCart.py
+++ jrm33/ModelCart.py
@@ -1,10 +1,10 @@
 import numpy as np
-from .Model import Model,ModelScalar
+from .Model import Model
 
-def ModelCart(x,y,z,MaxDeg=10):
+def ModelCart(x,y,z,Deg=13):
 	'''
-	JRM09 Magnetic field model (see Connerney et al 2018 below). The 
-	model uses right-handed System III coordinates (I think).  
+	JRM33 Magnetic field model (see Connerney et al 2022 below). The 
+	model uses right-handed System III coordinates (I think). 
 	
 	Inputs
 	======
@@ -14,7 +14,9 @@ def ModelCart(x,y,z,MaxDeg=10):
 		y-coordinate in Rj, R-H System III.
 	z : float
 		z-coordinate in Rj, R-H System III.
-		
+	Deg : int
+		Maximum degree of the model, valid 1 - 30 (default = 13).
+				
 	Returns
 	=======
 	Bx : float
@@ -24,12 +26,13 @@ def ModelCart(x,y,z,MaxDeg=10):
 	Bz : float
 		z component of magnetic field, nT.
 		
-	If using the JRM09 model, please cite the following paper:
+	If using the JRM33 model, please cite the following paper:
 	
-	Connerney, J. E. P., Kotsiaros, S., Oliversen, R. J., Espley, J. R., 
-	Joergensen, J. L., Joergensen, P. S., et al. (2018). A new model of 
-	Jupiter's magnetic field from Juno's first nine orbits. Geophysical 
-	Research Letters, 45, 2590– 2596. https://doi.org/10.1002/2018GL077312
+	Connerney, J. E. P., Timmins, S., Oliversen, R. J., Espley, J. R., 
+	Joergensen, J. L., Kotsiaros, S., et al. (2022). A new model of 
+	Jupiter's magnetic field at the completion of Juno's Prime Mission. 
+	Journal of Geophysical Research: Planets, 127, e2021JE007055. 
+	https://doi.org/10.1029/2021JE007055
 	
 	'''	
 	
@@ -53,54 +56,3 @@ def ModelCart(x,y,z,MaxDeg=10):
 	return Bx,By,Bz
 	
 
-def ModelCartScalar(x,y,z,MaxDeg=10):
-	'''
-	JRM09 Magnetic field model (see Connerney et al 2018 below). The 
-	model uses right-handed System III coordinates (I think).  
-	
-	Inputs
-	======
-	x : float
-		x-coordinate in Rj, R-H System III.
-	y : float
-		y-coordinate in Rj, R-H System III.
-	z : float
-		z-coordinate in Rj, R-H System III.
-		
-	Returns
-	=======
-	Bx : float
-		x component of magnetic field, nT.
-	By : float
-		y component of magnetic field, nT.
-	Bz : float
-		z component of magnetic field, nT.
-		
-	If using the JRM09 model, please cite the following paper:
-	
-	Connerney, J. E. P., Kotsiaros, S., Oliversen, R. J., Espley, J. R., 
-	Joergensen, J. L., Joergensen, P. S., et al. (2018). A new model of 
-	Jupiter's magnetic field from Juno's first nine orbits. Geophysical 
-	Research Letters, 45, 2590– 2596. https://doi.org/10.1002/2018GL077312
-	
-	'''	
-	
-	#convert to spherical polar coords
-	r = np.sqrt(x**2 + y**2 + z**2)
-	theta = np.arccos(z/r)
-	phi = (np.arctan2(y,x) + (2*np.pi)) % (2*np.pi)
-	
-	#call the model
-	Br,Bt,Bp = ModelScalar(r,theta,phi,MaxDeg)
-
-	#convert to Cartesian (hopefully correctly...)
-	cost = np.cos(theta)
-	sint = np.sin(theta)
-	cosp = np.cos(phi)
-	sinp = np.sin(phi)
-	Bx = Br*sint*cosp + Bt*cost*cosp - Bp*sinp
-	By = Br*sint*sinp + Bt*cost*sinp + Bp*cosp
-	Bz = Br*cost - Bt*sint
-	
-	return Bx,By,Bz
-	
